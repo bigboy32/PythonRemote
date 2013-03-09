2b72d01b08ff5a770bc4d60caa500252db65127a
Vel0x <DaleMyers19@gmail.com>
2013-03-09 11:23:22 +0000

    Added first plugin template

---
+++ plugins/PowerManager.py
@@ -0,0 +1,8 @@
+class PowerManager(object):
+'''The first example class for the python powered remote interface for my computer'''
+
+	id = "power_manager"
+	
+	def run(args):
+		print args
+		#args is a dictionary
