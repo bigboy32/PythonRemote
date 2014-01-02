148d434e9037c0704f3ef1351a1f6588ab2a500f
Vel0x <DaleMyers19@gmail.com>
2014-01-02 13:41:35 +0000

    Improved power managers errors

--- Plugins/PowerManager.py
+++ Plugins/PowerManager.py
@@ -1,4 +1,5 @@
 from Plugin import *
+import SystemInfo
 
 class PowerManager(Plugin):
     '''The first example class for the python powered remote interface for my computer'''
@@ -10,11 +11,25 @@ class PowerManager(Plugin):
 
     def run(self,callback,args):
         #args is a dictionary of commands and data
-        #We can't do the callback after it is asleep/shutdown/etc.
-        callback(self,0,{"answer":"Message received. Taking action...."})
         
         if args['option'] == 'sleep':
-            os.system("osascript -e 'tell application \"System Events\" to sleep'")
+            if SystemInfo.isMac():
+                callback(self,0,{"answer":"Message received. Sleeping..."})
+                os.system("osascript -e 'tell application \"System Events\" to sleep'")
+            else:
+                callback(self,2,{"answer":"Message received. Unsupported OS"})
+                Logger().error("Unsupported OS for command 'sleep'")
+        elif args['option'] == 'shutdown':
+            callback(self,1,{"answer":"Message received. Unsupported command"})
+            Logger().error("Shutdown not yet implemented")
+            raise NotImplementedError("Not Implemented")
+        elif args['option'] == 'restart':
+            callback(self,1,{"answer":"Message received. Unsupported command"})
+            Logger().error("Restart not yet implemented")
+            raise NotImplementedError("Not Implemented")
+        else:
+            callback(self,1,{"answer":"Message received. Unsupported command"})
+            Logger().error("Unsupported command '" + args['option'] + "'")
         
         #TODO: shutdown, restart
     
